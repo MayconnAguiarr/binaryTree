8fcaad9e10db972899e4349e82ea6b96f10ed023
Maycon <mayconn.aguiarr@gmail.com>
2018-03-14 22:42:41 -0300

    adicionado estrutura binaryTree

--- src/binaryTree/No.java
+++ src/binaryTree/No.java
@@ -2,4 +2,70 @@ package binaryTree;
 
 public class No {
 
+	private int valor;
+	private No esquerda;
+	private No direita;
+
+	No(int valor) {
+		this.valor = valor;
+		this.esquerda = null;
+		this.direita = null;
+	}
+
+	public static void montaArvore(No novo, No arvore) {
+		if (arvore == null) {
+			arvore = novo;
+		} else {
+			if (novo.valor < arvore.valor) {
+				if (arvore.esquerda == null) {
+					arvore.esquerda = novo;
+				} else {
+					montaArvore(novo, arvore.esquerda);
+				}
+			} else {
+				if (arvore.direita == null) {
+					arvore.direita = novo;
+				} else {
+					montaArvore(novo, arvore.direita);
+				}
+			}
+		}
+	}
+
+	public static void imprimirPreOrdem(No n) {
+		System.out.print(n.valor + " ");
+		
+		if (n.esquerda != null) {
+			imprimirPreOrdem(n.esquerda);
+		}
+		if (n.direita != null) {
+			imprimirPreOrdem(n.direita);
+		}
+	}
+
+	public static void imprimirEmOrdem(No n) {
+		if (n.esquerda != null) {
+			imprimirEmOrdem(n.esquerda);
+		}
+
+		System.out.print(n.valor + " ");
+
+		if (n.direita != null) {
+			imprimirEmOrdem(n.direita);
+		}
+	}
+
+	public static void imprimirPosOrdem(No n) {
+		if (n.esquerda != null) {
+			imprimirPosOrdem(n.esquerda);
+		}
+
+		if (n.direita != null) {
+			imprimirPosOrdem(n.direita);
+		}
+
+		System.out.print(n.valor + " ");
+
+	}
+
 }
--- src/binaryTree/Teste.java
+++ src/binaryTree/Teste.java
@@ -3,8 +3,41 @@ package binaryTree;
 public class Teste {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		No arvore =  new No(10);
+		No.montaArvore(arvore, null);
+		
+		No n5 = new No(5);
+		No.montaArvore(n5, arvore);
+
+		No n20 = new No(20);
+		No.montaArvore(n20, arvore);
+
+		No n33 = new No(33);
+		No.montaArvore(n33, arvore);
+		
+		No n1 = new No(1);
+		No.montaArvore(n1, arvore);
+		
+		No n2 = new No(2);
+		No.montaArvore(n2, arvore);
+
+		No n7 = new No(7);
+		No.montaArvore(n7, arvore);		
+		
+		No n30 = new No(30);
+		No.montaArvore(n30, arvore);
+
+		System.out.println("Pre-ordem");
+		No.imprimirPreOrdem(arvore);
+		System.out.println(" ");
+		
+		System.out.println("Em-ordem");
+		No.imprimirEmOrdem(arvore);
+		System.out.println(" ");
+
+		System.out.println("Pos-ordem");
+		No.imprimirPosOrdem(arvore);
+		System.out.println(" ");				
 	}
-
 }
